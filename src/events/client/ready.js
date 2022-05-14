6fb071df40d054fcf3ce121e3370d66060e2978f
danodee <92238288+danodee@users.noreply.github.com>
2022-05-14 21:02:15 +0530

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,20 +1,21 @@
 // Advanced djs slash command handler made with ❤️ by dano! [github.com/danodee]
 const { green, white, cyanBright } = require("chalk");
 const config = require("@config");
+const client = require("@root");
 
 module.exports = {
     name: "ready",
     once: true,
     execute: async() => {
-        
+
         const activites = config.activites;
         let activity = 0;
         await client.user.setPresence({ status: config.presence.status, activity: config.presence.activites[0] });
+
         setInterval(() => {
             activity++
             client.user.setActivity(activites[Math.floor(Math.random() * activites.length)])
         }, 120000);
-        
         console.log(white.bold("[") + green.bold("STATUS") + white.bold("] ") + cyanBright.bold("ONLINE"));
     },
 };
