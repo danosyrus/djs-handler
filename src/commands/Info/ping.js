eb0b3c1b9cb5036d1d4eb00405eb2fa695902f50
danodee <92238288+danodee@users.noreply.github.com>
2022-05-16 07:05:21 +0530

    Update ping.js

--- src/commands/Info/ping.js
+++ src/commands/Info/ping.js
@@ -1,7 +1,7 @@
 // Advanced djs slash command handler made with ❤️ by dano! [github.com/danodee]
 module.exports = {
     name: "ping",
-    owner: false,
+    staff: false,
     description: "Description Goes Here.",
     userpermissions: ["USE_APPLICATION_COMMANDS", "VIEW_CHANNEL"],
     botpermissions: ["SEND_MESSAGES", "VIEW_CHANNEL"],
