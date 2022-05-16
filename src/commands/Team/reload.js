a447f390d9a6fa35d2ec6935bb2c05ae26af7425
danodee <92238288+danodee@users.noreply.github.com>
2022-05-16 07:06:47 +0530

    Update reload.js

--- src/commands/Team/reload.js
+++ src/commands/Team/reload.js
@@ -1,1 +1,11 @@
+// Advanced djs slash command handler made with ❤️ by dano! [github.com/danodee]
+module.exports = {
+    name: "reload",
+    team: true,
+    description: "Reloads the bot.",
+    userpermissions: ["USE_APPLICATION_COMMANDS", "VIEW_CHANNEL"],
+    botpermissions: ["SEND_MESSAGES", "VIEW_CHANNEL"],
+    run: async(client, interaction) => {
 
+    },
+};
