759bd29d2b238ec0b5b25314af2788c07920ba20
danodee <92238288+danodee@users.noreply.github.com>
2022-05-17 19:43:13 +0530

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,4 +1,3 @@
-// Advanced djs slash command handler made with ❤️ by dano! [github.com/danodee]
 const { green, white, cyanBright } = require("chalk");
 const config = require("@config");
 const client = require("@root");
