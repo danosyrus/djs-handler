66ef25f9462a3204df195df6e17260cbde4151a9
dano <92238288+danodee@users.noreply.github.com>
2022-06-09 20:04:02 +0530

    Update main.js

--- src/database/main.js
+++ src/database/main.js
@@ -1,9 +1,10 @@
-const config = require("@config");
 const mongoose = require("mongoose");
+const config = require("@config");
+const client = require("@root");
 
 if (config.client.database.enabled === true) {
     mongoose.connect(config.client.database.url, {
         useNewUrlParser: true,
         useUnifiedTopology: true
-    });
+    }).then(client.utils.log("CONNECTED TO DATABASE"));
 };
