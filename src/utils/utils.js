c5d9031c42fb621acd1314d9b1f5e6fe5a06c128
dano <92238288+danodee@users.noreply.github.com>
2022-06-09 19:19:05 +0530

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,11 @@
+const { whiteBright, blueBright, greenBright } = require("chalk");
 const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
+
+module.exports.log = (args) => {
+    const date = `[${moment().format("DD-MM-YY ~ H:m:s")}]`
+    console.log(blueBright.bold(date) + whiteBright.bold(" = ") + greenBright.bold("[Log]: ") + whiteBright.bold(args))
+};
 
 module.exports.embed = async(embed) => {
     let newEmbed = new MessageEmbed()
