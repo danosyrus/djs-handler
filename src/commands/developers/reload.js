299581a21dd073a0e71f96db855a2c48e0c7fed7
dano <92238288+dano235@users.noreply.github.com>
2022-06-28 17:49:52 +0530

    Create reload.js

---
+++ src/commands/developers/reload.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "reload",
+    developersCmd: true,
+    description: "Reloads the bot.",
+    userpermissions: ["USE_APPLICATION_COMMANDS", "VIEW_CHANNEL"],
+    botpermissions: ["SEND_MESSAGES", "VIEW_CHANNEL"],
+    run: async(client, interaction) => {
+
+    },
+};
